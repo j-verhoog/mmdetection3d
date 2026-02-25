fe1f8b89a166d94b217d575e101f6f27a09bc91f
Jverhoog <jverhoog@yahoo.com>
2026-02-25 13:54:58 +0100

    only mom 0.001 running stats fcos

--- configs/fcos3d/fcos3d_r101_caffe_fpn_gn-head_dcn_2x8_1x_nus-mono3d_bn_unfrozen.py
+++ configs/fcos3d/fcos3d_r101_caffe_fpn_gn-head_dcn_2x8_1x_nus-mono3d_bn_unfrozen.py
@@ -7,9 +7,9 @@ model = dict(
     backbone=dict(
         dcn=dict(type='DCNv2', deform_groups=1, fallback_on_stride=False),
         stage_with_dcn=(False, False, True, True),
-        norm_eval=True,     # <-- True freezes BN stats (running mean and var), but not the affine parameters (scale and shift)
+        norm_eval=False,     # <-- True freezes BN stats (running mean and var), but not the affine parameters (scale and shift)
         frozen_stages=-1,     # <-- ADD THIS: Ensures no backbone stages are frozen
-        norm_cfg=dict(type='SyncBN', requires_grad=True)  # Unfreezes the BN scale and shift parameters, makes momentum10x smaller (, momentum=0.001) only for normeval=False
+        norm_cfg=dict(type='SyncBN', requires_grad=False, momentum=0.001)  # Unfreezes the BN scale and shift parameters, makes momentum10x smaller (, momentum=0.001) only for normeval=False
         ),)
 
 class_names = [
@@ -73,7 +73,7 @@ optimizer = dict(
         bias_lr_mult=2., 
         bias_decay_mult=0.,
         custom_keys={
-            'bn': dict(lr_mult=0.01)  # Applies a 0.01x LR multiplier ONLY to parameters with 'bn' in their name
+            'bn': dict(lr_mult=0.1)  # Applies a 0.1x LR multiplier ONLY to parameters with 'bn' in their name
         }
     )
 )
