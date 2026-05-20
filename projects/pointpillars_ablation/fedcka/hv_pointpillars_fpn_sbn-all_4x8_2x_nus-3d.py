fd3e44e952505b25af643e9c5d4ea12dfdcf456c
Jverhoog <jverhoog@yahoo.com>
2026-05-20 14:47:38 +0200

    fedavg + fedcka for pointpilalrs reworked config files

---
+++ projects/pointpillars_ablation/fedcka/hv_pointpillars_fpn_sbn-all_4x8_2x_nus-3d.py
@@ -0,0 +1,28 @@
+_base_ = [
+    '../_base_/models/hv_pointpillars_fpn_nus.py',
+    '../_base_/datasets/nus-3d.py', '../_base_/schedules/schedule_2x.py',
+    '../_base_/default_runtime.py'
+]
+
+# 1. Tell MMDet3D to import your CMT project
+custom_imports = dict(
+    imports=['projects.pointpillars_ablation.hooks.disable_gt_sampling', 'projects.pointpillars_ablation.hooks.force_stop', 'projects.pointpillars_ablation.hooks.fedselect_local'], 
+    allow_failed_imports=False)
+
+custom_hooks = [
+    dict(
+        type='DisableGTImportanceHook',
+        disable_after_epoch=999          # old placeholder; does not affect anything
+    ),
+    # Add this new hook:
+    dict(
+        type='ForceStopHook',
+        stop_epoch=999  # Placeholder; will be overwritten by sbatch
+    ),
+    dict(
+        type='FedSelectLocalTrainingHook',
+        mask_path=f'work_dirs/fedselect_masks/placeholder_mask.pth',
+        global_lr_scale=0.01,           # The paper's recommended gamma_u scaling
+        personal_lr_scale=1.0
+    ),
+]
