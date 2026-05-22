7a1350dd5eed5a0c319f9b43f9098aa6666059b3
Jverhoog <jverhoog@yahoo.com>
2026-05-22 10:51:10 +0200

    verbose nuscenes eval

--- mmdet3d/datasets/nuscenes_dataset.py
+++ mmdet3d/datasets/nuscenes_dataset.py
@@ -444,7 +444,7 @@ class NuScenesDataset(Custom3DDataset):
             result_path=result_path,
             eval_set=eval_set_map[self.version],
             output_dir=output_dir,
-            verbose=False)
+            verbose=True)
         nusc_eval.main(render_curves=False)
 
         # record metrics
